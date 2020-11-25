9a6d6ab3f950ac8554f53e8bca88bced1fd85f3c
Gerard Klijs <g.klijs@gmail.com>
2020-11-26 00:20:00 +0100

    Fix build

--- graphql-endpoint/src/main/java/nl/openweb/graphql_endpoint/properties/KafkaProperties.java
+++ graphql-endpoint/src/main/java/nl/openweb/graphql_endpoint/properties/KafkaProperties.java
@@ -5,13 +5,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotEmpty;
-
 @Data
 @Validated
 @Component
 @ConfigurationProperties(prefix = "kafka")
 public class KafkaProperties {
-    @NotEmpty
     private String commandsTopic;
 }
