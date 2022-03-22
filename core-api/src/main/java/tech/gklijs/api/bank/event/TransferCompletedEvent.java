a91d04e2f3741939735b0b2dc97a77226c12d0ef
Gerard Klijs <gerard.klijs@axoniq.io>
2022-03-22 20:33:14 +0100

    Use Jackson to serialize the events.

--- core-api/src/main/java/tech/gklijs/api/bank/event/TransferCompletedEvent.java
+++ core-api/src/main/java/tech/gklijs/api/bank/event/TransferCompletedEvent.java
@@ -1,9 +1,14 @@
 package tech.gklijs.api.bank.event;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 @Value
+@NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
+@AllArgsConstructor
 public class TransferCompletedEvent {
     @TargetAggregateIdentifier
     String transferId;
--- core-api/src/main/java/tech/gklijs/api/user/event/UserAccountCreatedEvent.java
+++ core-api/src/main/java/tech/gklijs/api/user/event/UserAccountCreatedEvent.java
@@ -1,9 +1,14 @@
 package tech.gklijs.api.user.event;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 @Value
+@NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
+@AllArgsConstructor
 public class UserAccountCreatedEvent {
     @TargetAggregateIdentifier
     String username;
