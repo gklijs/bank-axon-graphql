64b1a046f6c1a7bd7e6d3f1eda3c3ada608e024a
Gerard Klijs <gerard.klijs@axoniq.io>
2022-03-31 11:41:44 +0200

    Fix encoding and docker ignore issues

--- initialize.sh
+++ initialize.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+docker-compose -f docker-initializer.yml up -d &&
+sleep 10 &&
 for i in 1 2 3 4 5 6 7 8 9 10; do
-  docker-compose -f docker-initializer.yml up
   failures=$(docker inspect -f '{{ .State.ExitCode }}' initializer)
   if [[ "$failures" == "0" ]]; then
     echo -e "Successful initialization in loop ${i}"
     exit 0
   fi
-  sleep 3
+  sleep 5
 done
 exit 1
